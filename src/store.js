1d05c32658aabdaea9ebdc93a7618dd6a956e43f
Kim sanggyung <ksang0906@gmail.com>
2022-03-25 10:15:13 +0900

    사용자 정보를 관리할 store

---
+++ src/store.js
@@ -0,0 +1,9 @@
+import { writable } from "svelte/store";
+
+export let name = writable(0)
+
+export let birth = writable(0)
+
+export let idPicture = writable(0)
+
+export let pictureAlt = writable(0)
