7687034d9fdd088bbc09138c51f83d297506674c
Kim sanggyung <ksang0906@gmail.com>
2022-03-25 10:16:30 +0900

    화면애 그려질 부모 컴포넌트

---
+++ src/routes/index.svelte
@@ -0,0 +1,17 @@
+<script>
+    import "../app.css";
+    // import { time } from './main.js';
+	import Login from "./LogIn.svelte";
+    import Editmode from "./editmode.svelte";
+	
+</script>
+
+<body>
+
+	<main>
+
+		<Login/>
+
+	</main>
+</body>
+
