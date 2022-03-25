0fda6b3ea67971870e13e11fb82ca509771a2676
Kim sanggyung <ksang0906@gmail.com>
2022-03-25 15:41:18 +0900

    블라우저에서 구동하도록 조건 부여

--- src/routes/editmode.svelte
+++ src/routes/editmode.svelte
@@ -1,5 +1,5 @@
 <script>
-
+ 	import { browser } from '$app/env';
     import { name, idPicture, birth, pictureAlt } from "../store.js";
     
     let	inputName = ''
@@ -15,15 +15,15 @@
 		idPicture.set(inputPicture)
 		pictureAlt.set(inputAlt)
 		console.log(name)
-		localStorage.setItem('name', (inputName))
-		localStorage.setItem('birth', (inputBirth))
-		localStorage.setItem('img', (inputPicture))
-		localStorage.setItem('alt', (inputAlt))
-        editMode.editModeIn = !editMode.editModeIn
+		if(browser){
+			localStorage.setItem('name', (inputName))
+			localStorage.setItem('birth', (inputBirth))
+			localStorage.setItem('img', (inputPicture))
+			localStorage.setItem('alt', (inputAlt)) 
+		}
+		editMode.editModeIn = !editMode.editModeIn
 		
 	}
-
-	const reflash = () => localStorage.clear();
 </script>
 
 {#if !editMode.editModeIn}
@@ -33,6 +33,7 @@
     <input bind:value={inputPicture} type="text" placeholder='시진주소를 입력해 주세요.'>
     <input bind:value={inputAlt} type="text" placeholder="사진소개를 써주세요.">
     <button on:click={change}>등록</button>
-    <button on:click={reflash}>초기화</button>
-
+	{#if browser}
+    <button on:click={localStorage.clear()}>초기화</button>
+	{/if}
 {/if}
--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -2,6 +2,7 @@
 
 	import { name, idPicture, birth, pictureAlt } from "../store.js";
 	import Editmode from "./editmode.svelte";
+	import { browser } from '$app/env';
 
 	const userId = 'a';
 	const passWord = 123;
@@ -27,21 +28,23 @@
 		}
 	}
 
-	let storageId = localStorage.getItem('ID')
+	let storageName, storageBirth, storageImg, storageAlt;
 
-	if(storageId !== null){
-		inputId = storageId
-		console.log(inputId)
+	if(browser){
+		let storageId = localStorage.getItem('ID')
+
+		if(storageId !== null){
+			inputId = storageId
+			console.log(inputId)
+		}
+		storageName = localStorage.getItem('name')
+		storageBirth = localStorage.getItem('birth')
+		storageImg = localStorage.getItem('img')
+		storageAlt = localStorage.getItem('alt')
 	}
-
-	let storageName = localStorage.getItem('name')
-	let storageBirth = localStorage.getItem('birth')
-	let storageImg = localStorage.getItem('img')
-	let storageAlt = localStorage.getItem('alt')
-
-
+	
 	function logIn(){
-		if(userId == inputId, passWord == inputPass, storageName == null, storageBirth == null, storageImg == null, storageAlt == null){
+		if(userId == inputId && passWord == inputPass && storageName == null && storageBirth == null && storageImg == null && storageAlt == null){
 			name.set('이씨')
 			birth.set('1968년 6월 23일')
 			idPicture.set('https://ww.namu.la/s/91986ffc01b6136fb453c4ff8b3e63adf90e525e6b10e1643058339c924830276d1fc646c2ac918d1f31e83c59308ea232acb64afb280dabeee0afc024b9dd3063662e1cc7dd5e8191c10ae35b0c811b')
@@ -51,7 +54,7 @@
 			console.log(inputId)
 		}
 
-		else if(userNo2 == inputId, passNo2 == inputPass){
+		else if(userNo2 == inputId && passNo2 == inputPass){
 			name.set('정씨')
 			birth.set('1968년 9월 19일')
 			idPicture.set('https://w.namu.la/s/2b83516ecb2360d95faa10f85763ad59ade34ede897311db5fc3a40256d7f1dd855bbf8eddb57c9fff4d4a24d070fd5ad625181926ea33915713e2351be1486fd1d510c24e5d05bc1f220991ec051010')
@@ -60,7 +63,7 @@
 			console.log('로그인 성공')
 		}
 
-		else if (userId == inputId, passWord == inputPass, storageName !== null, storageBirth !== null, storageImg !== null, storageAlt !== null){
+		else if (browser && userId == inputId && passWord == inputPass && storageName !== null && storageBirth !== null && storageImg !== null && storageAlt !== null){
 			name.set(storageName)
 			birth.set(storageBirth)
 			idPicture.set(storageImg)
@@ -79,7 +82,7 @@
     const CLICKCHECK = () => check.checked = !check.checked;
 
 	function saveId(){
-        if(check.checked){
+        if(check.checked && browser){
             localStorage.setItem('ID', inputId)
         }
     }
@@ -91,6 +94,7 @@
 
 		{#if !logged.loggedIn}
 			<div class="loggedIn">
+				<h1 class="text-2xl">로ㄹㄹㄹ그인</h1>
 				<input bind:value={inputId} type="text" placeholder='아이디'>
 				<input bind:value={inputPass} type="password" placeholder='비밀번호'> 
 				<button on:click={saveId} on:click={logIn}>로그인</button>
