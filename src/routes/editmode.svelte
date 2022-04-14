0a8763164f9eeb088626148788fe3f938e53d6b3
Kim sanggyung <ksang0906@gmail.com>
2022-04-14 15:31:20 +0900

    필요없는 함수 제거 다른 컴포넌트에 사용하는 함수제거

--- src/routes/editmode.svelte
+++ src/routes/editmode.svelte
@@ -2,31 +2,26 @@
  	import { browser } from '$app/env';
     import { name, idPicture, birth, pictureAlt } from "../store.js";
     
-    let	inputName = ''
-	let inputBirth = ''
-	let	inputPicture = ''
-	let inputAlt =''
+    let	inputName = '';
+	let inputBirth = '';
+	let	inputPicture = '';
+	let inputAlt = '';
 	
-	let editMode = {editModeIn: false}
 
 	const change = () => {
-		name.set(inputName)
-		birth.set(inputBirth)
-		idPicture.set(inputPicture)
-		pictureAlt.set(inputAlt)
-		console.log(name)
+		name.set(inputName);
+		birth.set(inputBirth);
+		idPicture.set(inputPicture);
+		pictureAlt.set(inputAlt);
+		console.log(name);
 		if(browser){
-			localStorage.setItem('name', (inputName))
-			localStorage.setItem('birth', (inputBirth))
-			localStorage.setItem('img', (inputPicture))
-			localStorage.setItem('alt', (inputAlt)) 
-		}
-		editMode.editModeIn = !editMode.editModeIn
-		
+			localStorage.setItem('name', (inputName));
+			localStorage.setItem('birth', (inputBirth));
+			localStorage.setItem('img', (inputPicture));
+			localStorage.setItem('alt', (inputAlt)); 
+		}		
 	}
 </script>
-
-{#if !editMode.editModeIn}
     
     <input bind:value={inputName} type="text" placeholder='이름을 입력해 주세요.'> 
     <input bind:value={inputBirth} type="text" placeholder='생년월일을 입력해 주세요'>
@@ -36,4 +31,3 @@
 	{#if browser}
     <button on:click={localStorage.clear()}>초기화</button>
 	{/if}
-{/if}
