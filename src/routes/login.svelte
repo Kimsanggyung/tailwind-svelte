d2ddc26f3f8daa296eac71734ec500ce03175bfa
Kim sanggyung <ksang0906@gmail.com>
2022-04-14 15:31:43 +0900

    login컴포넌트 재구성

--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -1,115 +1,77 @@
 <script>
-
 	import { name, idPicture, birth, pictureAlt } from "../store.js";
 	import Editmode from "./editmode.svelte";
 	import { browser } from '$app/env';
 	import "../app.css";
-
+	
 	const userData = [
 		{ 
 			id: 'a', pwd: '123',
 			name: '이씨', birth: '1968년 6월 23일',
-			img: 'a.jpeg', imgAlt: 'Lee Jae-yong'
+			img: 'Mr.Lee.jpeg', imgAlt: 'Lee Jae-yong'
 		},
 		{ 
 			id: 'b', pwd: '345',
-			name: '정씨', birth: '1968년 9월 19일',
-			img: 'b.jpeg', imgAlt: 'Chung Yong-jin'
+      		name: '정씨', birth: '1968년 9월 19일',
+			img: 'Mr.chung.jpeg', imgAlt: 'Chung Yong-jin'
 		}
 	];
 
-	let inputId='';
-	let inputPass=';'
+	let check = {checked: false}
+  	let logged = false;
+	let editMode = false;
+  	let errored = null;
+	let inputId = '';
+	let inputPass = '';
 
-	console.log(inputId)
+	const CLICKCHECK = () => check.checked = !check.checked;
 
-	let logged = {loggedIn: false}
+  	const editToggle = () => editMode = !editMode;
 	
-	let editMode = {editModeIn: false}
-
-	const editToggle = () => editMode.editModeIn = !editMode.editModeIn
-
 	const logoutButton = () =>{
-		logged.loggedIn = !logged.loggedIn;
-		if(editMode.editModeIn){
-			editMode.editModeIn = !editMode.editModeIn
+		logged = !logged;
+		if(editMode){
+			editMode = !editMode
 		}
 	}
-
-	let storageName, storageBirth, storageImg, storageAlt;
-
+  
 	if(browser){
-		let storageId = localStorage.getItem('ID')
-
+		let storageId = localStorage.getItem('ID');
 		if(storageId !== null){
 			inputId = storageId
 			console.log(inputId)
 		}
-		
-		storageName = localStorage.getItem('name')
-		storageBirth = localStorage.getItem('birth')
-		storageImg = localStorage.getItem('img')
-		storageAlt = localStorage.getItem('alt')
+
 	}
+
+  const checkUser = (id, pwd) => {
+    const findUser = userData.find(user => user.id === id && user.pwd === pwd)
+    return findUser;
+  }
 	
-	function logIn(){
-		if(check.checked){
-            localStorage.setItem('ID', inputId)
-			console.log(inputId)
-        }
+	const logIn = () => {
+    if(check.checked) localStorage.setItem('ID', inputId);
 
-		else if(userId == inputId && passWord === inputPass && storageName == null && storageBirth == null && storageImg == null && storageAlt == null){
-			name.set('이씨')
-			birth.set('1968년 6월 23일')
-			idPicture.set('https://ww.namu.la/s/91986ffc01b6136fb453c4ff8b3e63adf90e525e6b10e1643058339c924830276d1fc646c2ac918d1f31e83c59308ea232acb64afb280dabeee0afc024b9dd3063662e1cc7dd5e8191c10ae35b0c811b')
-			pictureAlt.set('Lee Jae-yong')
-			logged.loggedIn = !logged.loggedIn,
-			console.log('로그인 성공')
-			console.log(inputId)
-		}
+    const findUser = checkUser(inputId, inputPass);
 
-		else if(userNo2 == inputId && passNo2 == inputPass){
-			name.set('정씨')
-			birth.set('1968년 9월 19일')
-			idPicture.set('https://w.namu.la/s/2b83516ecb2360d95faa10f85763ad59ade34ede897311db5fc3a40256d7f1dd855bbf8eddb57c9fff4d4a24d070fd5ad625181926ea33915713e2351be1486fd1d510c24e5d05bc1f220991ec051010')
-			pictureAlt.set('Chung Yong-jin')
-			logged.loggedIn = !logged.loggedIn
-			console.log('로그인 성공')
-		}
-
-		else if (browser && userId == inputId && passWord == inputPass && storageName !== null && storageBirth !== null && storageImg !== null && storageAlt !== null){
-			name.set(storageName)
-			birth.set(storageBirth)
-			idPicture.set(storageImg)
-			pictureAlt.set(storageAlt)
-			logged.loggedIn = !logged.loggedIn
-			console.log('로그인 성공')
-		}
-
-		else{
-			document.getElementById('error').innerHTML = "아이디 또는 비밀번호가 옳지않습니다."
-		}
-	}
-
-	let check = {checked: false}
-
-    const CLICKCHECK = () => check.checked = !check.checked;
-
-	console.log(inputId)
-
-	function saveId(){
-        if(check.checked && browser){
-            localStorage.setItem('ID', inputId)
-			console.log(inputId)
-        }
+    if(findUser){ //login 가능한 상태
+		name.set(findUser.name);
+      	birth.set(findUser.birth);
+      	idPicture.set(findUser.img);
+      	pictureAlt.set(findUser.imgAlt);
+      	logged = true;
+      	errored =false;
+	} else {
+      errored = true;
     }
+}
 
 </script>
 <body>
 
 	<main>
 
-		{#if !logged.loggedIn}
+		{#if !logged}
 			<div class="flex items-center justify-center min-h-screen bg-gray-100">
 				<div class="px-8 py-6 mt-4 text-left bg-white shadow-lg">
 					<h3 class="text-2xl font-bold text-center">로그인</h3>
@@ -122,11 +84,11 @@
 								<input bind:value={inputPass} type="password" placeholder="비밀번호"
 									class="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600">
 							</div>
-							{#if userNo2 !== inputId && passNo2 !== inputPass && userId !== inputId && passWord !== inputPass}
+							{#if errored }
 								<span class='text-red-600'>아이디 또는 비밀번호가 옳지않습니다.</span>
 							{/if}
 							<div class="flex items-baseline justify-between">
-								<button onclick={logIn} class="px-6 py-2 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900">로그인</button>
+								<button on:click={logIn} class="px-6 py-2 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900">로그인</button>
 								<label><input type="checkbox" on:click={CLICKCHECK} class="text-sm text-blue-600 hover:underline">아이디 저장</label>
 							</div>
 						</div>
@@ -134,7 +96,7 @@
 			</div>
 		{/if}
 
-		{#if logged.loggedIn}
+		{#if logged}
 			<div class="loggedIn">
 				<h1>환영합니다 {$name} 회원님</h1>
 				<h2>생년월일: {$birth} </h2>
@@ -144,7 +106,7 @@
 			</div>
 		{/if}
 
-		{#if editMode.editModeIn}
+		{#if editMode}
 			<Editmode/>
 		{/if}
 
@@ -153,10 +115,8 @@
 </body>
 
 <style>
-
 #Lee{
 	width: 175px;
 	height: 269px;
 }
-
 </style>
