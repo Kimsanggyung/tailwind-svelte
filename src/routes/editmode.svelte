c43ae875d604c3433448bde3c102e5440aea665f
Kim sanggyung <ksang0906@gmail.com>
2022-04-14 17:16:58 +0900

    함수 기능추가 등록을 누르면 입력이 사라지도록

--- src/routes/editmode.svelte
+++ src/routes/editmode.svelte
@@ -6,6 +6,7 @@
 	let inputBirth = '';
 	let	inputPicture = '';
 	let inputAlt = '';
+	let editMode = false;
 	
 
 	const change = () => {
@@ -14,6 +15,7 @@
 		idPicture.set(inputPicture);
 		pictureAlt.set(inputAlt);
 		console.log(name);
+		editMode = !editMode
 		if(browser){
 			localStorage.setItem('name', (inputName));
 			localStorage.setItem('birth', (inputBirth));
@@ -22,12 +24,13 @@
 		}		
 	}
 </script>
-    
+    {#if !editMode}
     <input bind:value={inputName} type="text" placeholder='이름을 입력해 주세요.'> 
     <input bind:value={inputBirth} type="text" placeholder='생년월일을 입력해 주세요'>
     <input bind:value={inputPicture} type="text" placeholder='시진주소를 입력해 주세요.'>
     <input bind:value={inputAlt} type="text" placeholder="사진소개를 써주세요.">
     <button on:click={change}>등록</button>
+	{/if}
 	{#if browser}
     <button on:click={localStorage.clear()}>초기화</button>
 	{/if}
